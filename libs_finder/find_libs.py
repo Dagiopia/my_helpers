7bb2553471780d3cddcce0c0a0098cf36b0d327c
Dagiopia <dagiopia@gmail.com>
2017-04-16 15:26:02 +0300

    added lib rpath modifier

---
+++ libs_finder/find_libs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python 
+import os, sys
+
+SEARCH_PATH = '/home/dagiopia/opencog_rasp/usr/local/'
+FAKE_ROOT = ['/home/dagiopia/opencog_rasp', '/home/dagiopia/rasp_root']
+
+
+def mod_rpath():
+        s_path = SEARCH_PATH
+        sin, sout = os.popen4(['find', '-P', s_path, '-type', 'f', '-name', '*.so'])
+        srs = sout.readlines()
+        srs = [srs[i][:-1] if srs[i].count('\n')>0 else srs[i] for i in range(len(srs))]
+	for f in srs:
+		sin, sout = os.popen4(['chmod', '+x', f])
+	#this seems redundant but is included so that not just shared libraries but
+	#executables like cogserver are included too.
+	#so make all shared libs executable then search for executables
+	sin, sout = os.popen4(['find', '-P', s_path, '-type', 'f', '-executable'])
+	srs = sout.readlines()
+	srs = [srs[i][:-1] if srs[i].count('\n')>0 else srs[i] for i in range(len(srs))]
+
+	for f in srs:
+		sin, sout = os.popen4(['chrpath', '-l', f])
+		res = sout.readlines()
+		res = res[0][res[0].index('RPATH=')+6:-1]
+		for fr in FAKE_ROOT:
+			res = res.replace(fr, '')
+		sin, sout = os.popen4(['chrpath', f, '-r', res])
+		chres = sout.readlines()
+		print chres
+
+
+if __name__ == "__main__":
+	mod_rpath()
